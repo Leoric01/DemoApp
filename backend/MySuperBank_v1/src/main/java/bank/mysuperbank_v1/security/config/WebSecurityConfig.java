3374915b8f1c35ae33215eae35006f02251b80e4
Leoric01 <troglodit.nick@seznam.cz>
2023-09-03 17:11:01 +0200

    deleted unwanted imports

--- backend/MySuperBank_v1/src/main/java/bank/mysuperbank_v1/security/config/WebSecurityConfig.java
+++ backend/MySuperBank_v1/src/main/java/bank/mysuperbank_v1/security/config/WebSecurityConfig.java
@@ -1,13 +1,9 @@
 package bank.mysuperbank_v1.security.config;
 
-
-import bank.mysuperbank_v1.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
